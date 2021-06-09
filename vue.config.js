bf59169bf0e0dfde2f49488f040903c665265294
zhixjin <zhixjin@microsoft.com>
2021-06-09 15:16:49 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const MonacoEditorWebpackPlugin = require("monaco-editor-webpack-plugin");
+
 module.exports = {
   lintOnSave: false,
+  configureWebpack: {
+    plugins: [new MonacoEditorWebpackPlugin()],
+  },
 };
